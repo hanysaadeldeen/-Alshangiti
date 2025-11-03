a7bf675e74c220d091fb36e80c984c075d2050df
Hany Mohamed <hanymohamedsaadeldean@gmail.com>
2025-11-03 10:12:43 +0200

    update history in about

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -37,7 +37,7 @@ export default defineNuxtConfig({
           href: "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css",
         },
       ],
-      title: "محمود الشنقيطي",
+      title: "محمود الشنقيطي للمحاماة والاستشارات القانونية",
       meta: [
         {
           name: "description",
