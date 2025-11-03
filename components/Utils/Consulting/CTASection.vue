a71023a643acc4096c552d0c0d2f504461e879c5
Hany Mohamed <hanymohamedsaadeldean@gmail.com>
2025-11-03 10:30:40 +0200

    Finish

--- components/Utils/Consulting/CTASection.vue
+++ components/Utils/Consulting/CTASection.vue
@@ -1,59 +1,77 @@
 <template>
-    <div class="mb-16 md:mb-[120px] py-24 lg:py-44  relative">
-        <div class="absolute top-0 left-0 w-full h-full ">
-            <img :src="img" :alt="title" class="w-full h-full  object-cover brightness-50">
-        </div>
-        <div class="mx-auto max-w-[1315px] max-2xl:px-6">
-
-            <div class=" max-w-[834px] relative z-10">
-                <h1 class="text-2xl md:text-3xl lg:text-[40px]  lg:!leading-[72px] text-white font-bold">{{ $t(title) }}
-                </h1>
-                <div class="flex gap-3 md:gap-6 justify-start items-center flex-wrap mt-8 md:mt-12">
-                    <nuxt-link to="contact-us">
-                        <button
-                            class="border-[2px] border-white border-transparent relative overflow-hidden  max-md:w-full min-w-[168px] w-fit  px-5 py-2 flex flex-col max-h-[46px] justify-center items-center gap-4 transition-all duration-300 cursor-pointer group ease-in-out bg-primary-500 text-white hover:bg-primary-600 active:bg-primary-700 ">
-                            <span
-                                class="text-sm lg:text-base font-bold group-hover:-translate-y-[200%] transition-all duration-300 cursor-pointer  ease-in-out text-white">
-                                {{ locale === 'ar' ? 'اطلب خدمة' : 'Request service' }}
-                            </span>
-                            <span
-                                class="absolute w-full -bottom-10 text-sm lg:text-base font-bold  group-hover:-bottom-0 group-hover:-translate-y-1/2  left-1/2 -translate-x-1/2 transition-all duration-300 cursor-pointer  ease-in-out text-secondary">
-                                {{ locale === 'ar' ? 'اطلب خدمة' : 'Request service' }}
-                            </span>
-                        </button>
-                    </nuxt-link>
-                    <nuxt-link to="contact-us">
-                        <button
-                            class="w-fit relative overflow-hidden border-[2px] max-md:w-full  min-w-[191px]  py-2 px-5  flex flex-col max-h-[46px] justify-center items-center gap-4 transition-all duration-300 cursor-pointer group ease-in-out bg-transparent text-white  hover:text-secondary border-white hover:border-secondary">
-                            <span
-                                class="  text-sm lg:text-base font-bold group-hover:-translate-y-[200%] transition-all duration-300 cursor-pointer  ease-in-out text-white">
-
-                                {{ locale === 'ar' ? 'احجز استشارة قانونية' : 'Schedule a Legal Consultation' }}
-                            </span>
-                            <span
-                                class="absolute -bottom-10  text-sm lg:text-base font-bold  group-hover:bottom-0 group-hover:-translate-y-1/2 w-full  left-1/2 -translate-x-1/2 transition-all duration-300 cursor-pointer  ease-in-out text-secondary">
-                                {{ locale === 'ar' ? 'احجز استشارة قانونية' : 'Schedule a Legal Consultation' }}
-
-                            </span>
-                        </button>
-                    </nuxt-link>
-                </div>
-            </div>
-        </div>
-
-
+  <div class="mb-16 md:mb-[120px] py-24 lg:py-44 relative">
+    <div class="absolute top-0 left-0 w-full h-full">
+      <img
+        :src="img"
+        :alt="title"
+        class="w-full h-full object-cover brightness-50"
+      />
     </div>
+    <div class="mx-auto max-w-[1315px] max-2xl:px-6">
+      <div class="max-w-[834px] relative z-10">
+        <h1
+          class="text-2xl md:text-3xl lg:text-[40px] lg:!leading-[72px] text-white font-bold"
+        >
+          {{ $t(title) }}
+        </h1>
+        <div
+          class="flex gap-3 md:gap-6 justify-start items-center flex-wrap mt-8 md:mt-12"
+        >
+          <nuxt-link to="contact-us">
+            <button
+              class="border-[2px] border-white border-transparent relative overflow-hidden max-md:w-full min-w-[168px] w-fit px-5 py-2 flex flex-col max-h-[46px] justify-center items-center gap-4 transition-all duration-300 cursor-pointer group ease-in-out bg-primary-500 text-white hover:bg-primary-600 active:bg-primary-700"
+            >
+              <span
+                class="text-sm lg:text-base font-bold group-hover:-translate-y-[200%] transition-all duration-300 cursor-pointer ease-in-out text-white"
+              >
+                {{ locale === "ar" ? "أطلب خدماتنا" : "Request service" }}
+              </span>
+              <span
+                class="absolute w-full -bottom-10 text-sm lg:text-base font-bold group-hover:-bottom-0 group-hover:-translate-y-1/2 left-1/2 -translate-x-1/2 transition-all duration-300 cursor-pointer ease-in-out text-secondary"
+              >
+                {{ locale === "ar" ? "أطلب خدماتنا" : "Request service" }}
+              </span>
+            </button>
+          </nuxt-link>
+          <nuxt-link to="contact-us">
+            <button
+              class="w-fit relative overflow-hidden border-[2px] max-md:w-full min-w-[191px] py-2 px-5 flex flex-col max-h-[46px] justify-center items-center gap-4 transition-all duration-300 cursor-pointer group ease-in-out bg-transparent text-white hover:text-secondary border-white hover:border-secondary"
+            >
+              <span
+                class="text-sm lg:text-base font-bold group-hover:-translate-y-[200%] transition-all duration-300 cursor-pointer ease-in-out text-white"
+              >
+                {{
+                  locale === "ar"
+                    ? "احجز استشارة قانونية"
+                    : "Schedule a Legal Consultation"
+                }}
+              </span>
+              <span
+                class="absolute -bottom-10 text-sm lg:text-base font-bold group-hover:bottom-0 group-hover:-translate-y-1/2 w-full left-1/2 -translate-x-1/2 transition-all duration-300 cursor-pointer ease-in-out text-secondary"
+              >
+                {{
+                  locale === "ar"
+                    ? "احجز استشارة قانونية"
+                    : "Schedule a Legal Consultation"
+                }}
+              </span>
+            </button>
+          </nuxt-link>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
-const { locale } = useI18n()
+const { locale } = useI18n();
 
 defineProps<{
-    img: string,
-    title: string
-    buttonO: string,
-    buttonT: string
-}>()
+  img: string;
+  title: string;
+  buttonO: string;
+  buttonT: string;
+}>();
 </script>
 
 <style scoped></style>
