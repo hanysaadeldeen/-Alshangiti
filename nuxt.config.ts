9e035139777792617632fa9516a371843c590508
hanysaadeldeen <hanymohamedsaadeldean@gmail.com>
2025-11-20 20:01:58 +0200

    remove  font-awesome for test Render blocking requests

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -42,10 +42,10 @@ export default defineNuxtConfig({
           href: "https://fonts.googleapis.com/css2?family=Cairo:wght@200..1000&display=swap",
           onload: "this.onload=null;this.rel='stylesheet'",
         },
-        {
-          rel: "stylesheet",
-          href: "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css",
-        },
+        // {
+        //   rel: "stylesheet",
+        //   href: "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css",
+        // },
         {
           rel: "preload",
           as: "image",
