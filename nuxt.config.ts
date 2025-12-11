8b30cb78363194be8c572cdeae3a3e3e5fc1a14e
Hany Mohamed <hanymohamedsaadeldean@gmail.com>
2025-12-11 11:07:06 +0200

    delete meta from global nuxt.config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -49,16 +49,16 @@ export default defineNuxtConfig({
           imagesizes: "100vw",
         },
       ],
-      title: "محمود الشنقيطي للمحاماة والاستشارات القانونية",
+      // title: "محمود الشنقيطي للمحاماة والاستشارات القانونية",
 
       meta: [
         { property: "og:image:width", content: "1200" },
         { property: "og:image:height", content: "630" },
-        {
-          name: "description",
-          content:
-            "محمود الشنقيطي وشركاه هو صرح قانوني رائد تأسس على يد محامٍ خبير أمضى أكثر من 20 عامًا في قلب المشهد القانوني السعودي",
-        },
+        // {
+        //   name: "description",
+        //   content:
+        //     "محمود الشنقيطي وشركاه هو صرح قانوني رائد تأسس على يد محامٍ خبير أمضى أكثر من 20 عامًا في قلب المشهد القانوني السعودي",
+        // },
         {
           property: "og:image",
           content: "https://shangiti.com/LogoTap.png",
